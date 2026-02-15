68f24b22ae419b1ca317fda25987077276671a41
03-Bunny-06 <saikowshik0306@gmail.com>
2026-02-15 10:12:26 +0530

    models-created

---
+++ server/models/transactionModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["Food", "Transport", "Rent", "Utilities", "Entertainment", "Shopping", "Income", "Miscellaneous","Other"],
+        required: true,
+        default: "Other"
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    notes: {
+        type: String,
+        trim: true,
+        maxLength: 150
+    }
+})
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
